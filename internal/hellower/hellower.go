20b0df642ff1d9e4d0b91843b96927877be6dff4
Michael Filanov <mfilanov@redhat.com>
2021-10-24 23:29:01 +0300

    add log line to get request

--- internal/hellower/hellower.go
+++ internal/hellower/hellower.go
@@ -2,6 +2,7 @@ package hellower
 
 import (
 	"context"
+	"log"
 	"simple/models"
 	"simple/restapi"
 	"simple/restapi/operations/hello"
@@ -14,5 +15,6 @@ var _ restapi.HelloAPI = (*Hellower)(nil)
 type Hellower struct{}
 
 func (h *Hellower) GetHello(ctx context.Context, params hello.GetHelloParams) middleware.Responder {
+	log.Println("Got get request")
 	return hello.NewGetHelloOK().WithPayload(&models.HelloReply{Reply: "world"})
 }
